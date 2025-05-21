463b3e0f868d3aa27c951d73845715e7d47a65b2
Alexandre Darocha <alexandre.darocha.vente@gmail.com>
2025-05-21 02:26:09 +0200

    ✅ Fix: update i18n.js to NextIntlClientProvider

--- i18n.js
+++ i18n.js
@@ -1,15 +1,16 @@
-// 📁 /i18n.js
-import { NextIntlProvider } from 'next-intl'
+// 📁 i18n.js
+
+import { NextIntlClientProvider, useMessages } from 'next-intl'
 import { notFound } from 'next/navigation'
-import { useMessages } from 'next-intl'
 
 export default function LocaleProvider({ children, locale }) {
   const messages = useMessages()
+
   if (!messages) notFound()
 
   return (
-    <NextIntlProvider locale={locale} messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       {children}
-    </NextIntlProvider>
+    </NextIntlClientProvider>
   )
 }
