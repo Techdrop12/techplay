2f22b5db1be8acfc3203429abbda9f0b84274ad2
Alexandre Darocha <alexandre.darocha.vente@gmail.com>
2025-05-22 00:15:52 +0200

    fix layout client structure

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,30 +1,7 @@
 import LocaleProvider from '../../../i18n'
 import '@/styles/globals.css'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import ClientWrapper from '../ClientWrapper'
-import useAnalytics from '../../lib/useAnalytics'
-import PromoBanner from '@/components/PromoBanner'
-import LiveChat from '@/components/LiveChat'
-import EmailCapturePopup from '@/components/EmailCapturePopup' // ✅ modifié ici
-import CartReminder from '@/components/CartReminder'
+import LayoutWithAnalytics from './LayoutWithAnalytics'
 import FreeShippingBadge from '@/components/FreeShippingBadge'
-
-function LayoutWithAnalytics({ children }) {
-  useAnalytics()
-
-  return (
-    <ClientWrapper>
-      <PromoBanner />
-      <EmailCapturePopup /> {/* ✅ modifié ici */}
-      <CartReminder />
-      <LiveChat />
-      <Header />
-      {children}
-      <Footer />
-    </ClientWrapper>
-  )
-}
 
 export default function LocaleLayout({ children, params: { locale } }) {
   return (
