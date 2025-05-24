41fa45c47da9bcf33792e736a00b2ef39f3ad630
Alexandre Darocha <alexandre.darocha.vente@gmail.com>
2025-05-24 14:04:10 +0200

    ✅ Fix final ClientWrapper + composants manquants pour Vercel

--- src/app/ClientWrapper.js
+++ src/app/ClientWrapper.js
@@ -4,8 +4,12 @@ import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import Script from 'next/script'
 import { Toaster } from 'react-hot-toast'
+
 import { CartProvider } from '@/context/cartContext'
-import { ThemeProvider } from '@/context/themeContext' // contexte thème (dark/light)
+import { ThemeProvider } from '@/context/themeContext'
+import { UpsellProvider } from '@/context/upsellContext'
+import PushPermission from '@/components/PushPermission'
+
 import { pageview, GA_ID } from '@/lib/ga4'
 import useHotjar from '@/lib/hotjar'
 
@@ -20,24 +24,27 @@ export default function ClientWrapper({ children }) {
   return (
     <ThemeProvider>
       <CartProvider>
-        {GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="gtag-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
-        {children}
-        <Toaster position="top-right" />
+        <UpsellProvider>
+          {GA_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="gtag-init" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}');
+                `}
+              </Script>
+            </>
+          )}
+          <PushPermission />
+          {children}
+          <Toaster position="top-right" />
+        </UpsellProvider>
       </CartProvider>
     </ThemeProvider>
   )
---
+++ src/components/FreeShippingBadge.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function FreeShippingBadge() {
+  return (
+    <div className="bg-yellow-200 text-yellow-900 px-3 py-1 text-xs rounded-full shadow-sm inline-block">
+      🚚 Livraison gratuite
+    </div>
+  )
+}
