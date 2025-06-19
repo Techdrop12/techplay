28e8dc447c004f1c2fa851783c2128af594595f0
Alexandre Darocha <alexandre.darocha.vente@gmail.com>
2025-06-19 20:53:11 +0200

    Corrige chemins import messages et lib blog

---
+++ src/lib/blog.js
@@ -0,0 +1,10 @@
+// src/lib/blog.js
+
+/**
+ * Fonction stub pour récupérer tous les articles de blog.
+ * À compléter avec ta vraie méthode de récupération (BDD, fichiers, CMS, etc.)
+ */
+export async function getAllPosts() {
+  // Exemple : retourne un tableau vide pour éviter l’erreur
+  return [];
+}
